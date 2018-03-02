ffc218884e829567c5c66e6bedb0aa91c02b6b1a
Robin Jayaswal <robin.e.jayaswal.18@dartmouth.edu>
2018-03-01 23:50:54 -0500

    cleanup

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ commands.forEach(function(command) {
   var buildUp = program
     .command(command.cmd)
     .description(command.description)
-    // .option("-s, --setup_mode [mode]", "Which setup mode to use")
   command.options.forEach(function(option) {
     buildUp.option(option.flags, option.description)
   })
@@ -40,9 +39,6 @@ commands.forEach(function(command) {
       } else {
         runCosmicCommand(command, invokedCmd)
       }
-      // load cosmic before this with the token? (like top of file.) check here if auth needed or not.
-      // how do we create callbacks through json. All things will have a method in cosmicjs-node?
-      // even so, we have to pass the args
     })
 })
 
@@ -80,145 +76,6 @@ function runCosmicCommand(command, invokedCmd) {
     console.log(err)
     process.exit(1)
   })
-
-  // if (!cosmicMethod.useBucket) {
-  //   if (!Cosmic[cosmicMethod.method]) {
-  //     print.error('Method ' + cosmicMethod.method + ' does not exist on Cosmic. Please report this at ' + colors.blue('https://github.com/cosmicjs/cosmic-cli/issues/'))
-  //     process.exit(1)
-  //   }
-  //
-  //
-  //   Cosmic[cosmicMethod.method](params).then(function(res){
-  //     print.success('Success')
-  //     console.log(res)
-  //     process.exit(0)
-  //   }).catch(function(err) {
-  //     print.error('Error:')
-  //     console.log(err)
-  //     process.exit(1)
-  //   })
-  // } else {
-  //   if (!bucket[cosmicMethod.method]) {
-  //     print.error('Method ' + cosmicMethod.method + ' does not exist on Cosmic. Please report this at ' + colors.blue('https://github.com/cosmicjs/cosmic-cli/issues/'))
-  //     process.exit(1)
-  //   }
-  //
-  //   Cosmic[cosmicMethod.method](params).then(function(res){
-  //     print.success('Success')
-  //     console.log(res)
-  //     process.exit(0)
-  //   }).catch(function(err) {
-  //     print.error('Error:')
-  //     console.log(err)
-  //     process.exit(1)
-  //   })
-  // }
 }
 
 program.parse(process.argv)
-
-// program
-//   .version('0.1.0')
-//   .arguments('<cmd>')
-//   .action(function (cmd) {
-//      cmdValue = cmd;
-//   });
-//
-// program.parse(process.argv);
-//
-// if (typeof cmdValue === 'undefined') {
-//   console.error('no command given!');
-//   process.exit(1);
-// }
-//
-// if (cmdValue === 'add-bucket') {
-// 	getToken(function(token) {
-// 		addBucket(token, function(res) {
-// 			console.log(res)
-// 		})
-// 	})
-// }
-//
-// function getToken(callback) {
-// 	var questions = [
-// 		{
-// 			type: 'text',
-// 			name: 'email',
-// 			message: 'Email:'
-// 		},
-// 		{
-// 			type: 'password',
-// 			name: 'password',
-// 			message: 'Password:'
-// 		}
-// 	]
-// 	inquirer.prompt(questions).then(function (answers) {
-// 		request.post({ url: 'https://api.cosmicjs.com/v1/authenticate', form: {email: answers.email, password: answers.password }}, function(err, httpResponse, body){
-// 			return callback(JSON.parse(body).token)
-// 		})
-// 	})
-// }
-//
-// function addBucket(token, callback) {
-// 	var questions = [
-// 		{
-// 			type: 'text',
-// 			name: 'title',
-// 			message: 'What is the title of your new Bucket?'
-// 		}
-// 	]
-// 	inquirer.prompt(questions).then(function (answers) {
-// 		var headers = {
-//       'Authorization': `Bearer ${token}`
-//     }
-// 		request.post({ headers, url: 'https://api.cosmicjs.com/v1/buckets', form: { title: answers.title } }, function(err, httpResponse, body){
-// 			return callback(JSON.parse(body))
-// 		})
-// 	})
-// }
-
-
-/* Example
-
-var program = require('commander');
-
-program
-  .version('0.1.0')
-  .option('-C, --chdir <path>', 'change the working directory')
-  .option('-c, --config <path>', 'set config path. defaults to ./deploy.conf')
-  .option('-T, --no-tests', 'ignore test hook');
-
-program
-  .command('setup [env]')
-  .description('run setup commands for all envs')
-  .option("-s, --setup_mode [mode]", "Which setup mode to use")
-  .action(function(env, options){
-    var mode = options.setup_mode || "normal";
-    env = env || 'all';
-    console.log('setup for %s env(s) with %s mode', env, mode);
-  });
-
-program
-  .command('exec <cmd>')
-  .alias('ex')
-  .description('execute the given remote cmd')
-  .option("-e, --exec_mode <mode>", "Which exec mode to use")
-  .action(function(cmd, options){
-    console.log('exec "%s" using %s mode', cmd, options.exec_mode);
-  }).on('--help', function() {
-    console.log('  Examples:');
-    console.log();
-    console.log('    $ deploy exec sequential');
-    console.log('    $ deploy exec async');
-    console.log();
-  });
-
-program
-  .command('*')
-  .action(function(env){
-    console.log('deploying "%s"', env);
-  });
-
-program.parse(process.argv);
-
-*/
