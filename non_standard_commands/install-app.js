1585cd5bd86b5f4499004d0b92a888b3655857ca
Robin Jayaswal <robin.e.jayaswal.18@dartmouth.edu>
2018-04-02 00:36:06 -0400

    install and run app working

--- non_standard_commands/install-app.js
+++ non_standard_commands/install-app.js
@@ -1,30 +1,31 @@
 var print = require('../lib/output')
 var request = require('request')
 var Cosmic = require('cosmicjs')
-var exec = require('child_process').exec;
+var exec = require('child_process').exec
 
-function handler(invokedCmd, bucket, token) {
-  var bucket = Cosmic().bucket({
+function handler(options, bucket, token) {
+  var invokedCmd = options.invokedCmd
+  var appBucket = Cosmic().bucket({
     slug: 'cosmicapps'
   })
-  bucket.getObject({
+  appBucket.getObject({
     slug: invokedCmd
-  }).then(data => {
-    var repo_url = data.object.metadata.repo;
-    print.cosmic('Cloning repo: ' + repo_url);
+  }).then(function(data) {
+    var repo_url = data.object.metadata.repo
+    print.cosmic('Cloning repo: ' + repo_url)
     exec('git clone ' + repo_url + ' ./' + invokedCmd, function(error, stdout, stderr) {
       if (error !== null) {
-          console.log('exec error: ' + error);
+        console.log('exec error: ' + error)
       } else {
         print.success('Success! Now you can run the start command to run this app:')
-        print.cosmic('cosmic start-app ' + invokedCmd);
+        print.cosmic('cosmic start-app ' + invokedCmd)
       }
-    });
-  }).catch(err => {
+    })
+  }).catch(function(err) {
     print.error('Error')
     console.log(err)
     process.exit(1)
-  });
+  })
 }
 
 module.exports = handler
--- non_standard_commands/start-app.js
+++ non_standard_commands/start-app.js
@@ -6,48 +6,39 @@ var exec = require('child_process').exec
 var bucketConfig = require('../lib/bucket_config')
 var bucket_opts = bucketConfig.getCosmicBucketOptions()
 
-function handler(invokedCmd) {
-  var bucket = Cosmic().bucket({
-    slug: 'cosmicapps'
-  })
-  bucket.getObject({
-    slug: invokedCmd
-  }).then(data => {
-    async.series([
-      function(callback) {
-        var install_command = 'cd ./' + invokedCmd + '; npm i;'
-        exec(install_command, function(error, stdout, stderr) {
-          if (error !== null) {
-              console.log('exec error: ' + error);
-          } else {
-            print.success('App deps installed');
-            callback();
-          }
-        });
-      },
-      function() {
-        var PORT = process.env.PORT || 3000;
-        console.log(bucket_opts.slug)
-        var start_command = 'cd ./' + invokedCmd + '; COSMIC_BUCKET=' + bucket_opts.slug + ' PORT=' + PORT + ' npm start;'
-        console.log(start_command)
-        print.success('Starting app on port number ' + PORT + ' connected to Bucket ' + bucket_opts.slug);
-        var child = exec(start_command, function(error, stdout, stderr) {
-          if (error !== null) {
-              console.log('exec error: ' + error);
-          } else {
-            print.success('App started!');
-          }
-        });
-        child.stdout.on('data', function(data) {
-          console.log(data);
-        });
-      }
-    ])
-  }).catch(err => {
-    print.error('Error')
-    console.log(err)
-    process.exit(1)
-  })
+function handler(options) {
+  var invokedCmd = options.invokedCmd
+  
+  async.series([
+    function(callback) {
+      var install_command = 'cd ./' + invokedCmd + '; npm i;'
+      exec(install_command, function(error, stdout, stderr) {
+        if (error !== null) {
+          console.log('exec error: ' + error)
+        } else {
+          print.success('App deps installed')
+          callback()
+        }
+      })
+    },
+    function() {
+      var PORT = process.env.PORT || 3000
+      console.log(bucket_opts.slug)
+      var start_command = 'cd ./' + invokedCmd + '; COSMIC_BUCKET=' + bucket_opts.slug + ' PORT=' + PORT + ' npm start;'
+      console.log(start_command)
+      print.success('Starting app on port number ' + PORT + ' connected to Bucket ' + bucket_opts.slug)
+      var child = exec(start_command, function(error, stdout, stderr) {
+        if (error !== null) {
+          console.log('exec error: ' + error)
+        } else {
+          print.success('App started!')
+        }
+      })
+      child.stdout.on('data', function(data) {
+        console.log(data)
+      })
+    }
+  ])
 }
 
 module.exports = handler
