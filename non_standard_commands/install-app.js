15c2dcbba47324f418e224e78cfd442af493fcec
Robin Jayaswal <robin.e.jayaswal.18@dartmouth.edu>
2018-04-05 14:47:30 -0400

    warning message on install

--- non_standard_commands/install-app.js
+++ non_standard_commands/install-app.js
@@ -2,6 +2,8 @@ var print = require('../lib/output')
 var request = require('request')
 var Cosmic = require('cosmicjs')
 var exec = require('child_process').exec
+var inquirer = require('inquirer')
+var bucketConfig = require('../lib/bucket_config')
 
 function handler(options, bucket, token) {
   var invokedCmd = options.invokedCmd
@@ -11,15 +13,31 @@ function handler(options, bucket, token) {
   appBucket.getObject({
     slug: invokedCmd
   }).then(function(data) {
-    var repo_url = data.object.metadata.repo
-    print.cosmic('Cloning repo: ' + repo_url)
-    exec('git clone ' + repo_url + ' ./' + invokedCmd, function(error, stdout, stderr) {
-      if (error !== null) {
-        console.log('exec error: ' + error)
-      } else {
-        print.success('Success! Now you can run the start command to run this app:')
-        print.cosmic('cosmic start-app ' + invokedCmd)
+    var bucketOpts = bucketConfig.getCosmicBucketOptions()
+
+    var questions = [
+      {
+        type: 'confirm',
+        name: 'confirm',
+        message: 'DANGER! Are you sure you want to overwrite all data and files in the Bucket: ' + bucketOpts.slug + '?',
+      },
+    ]
+    inquirer.prompt(questions).then(function(answers) {
+      if (!answers.confirm) {
+        print.error('Install Cancelled')
+        return process.exit(1)
       }
+
+      var repo_url = data.object.metadata.repo
+      print.cosmic('Cloning repo: ' + repo_url)
+      exec('git clone ' + repo_url + ' ./' + invokedCmd, function(error, stdout, stderr) {
+        if (error !== null) {
+          console.log('exec error: ' + error)
+        } else {
+          print.success('Success! Now you can run the start command to run this app:')
+          print.cosmic('cosmic start-app ' + invokedCmd)
+        }
+      })
     })
   }).catch(function(err) {
     print.error('Error')
