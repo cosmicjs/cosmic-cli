cc33d4eff7f791e744aacb3fc1587c71c7b7c561
Tony Spiro <tspiro@tonyspiro.com>
2018-04-06 23:30:42 -0500

    open demo link with frame

--- non_standard_commands/view-app.js
+++ non_standard_commands/view-app.js
@@ -4,14 +4,7 @@ var opn = require('opn')
 
 function handler(options) {
   var invokedCmd = options.invokedCmd
-  var appBucket = Cosmic().bucket({
-    slug: 'cosmicapps'
-  })
   var demoLink = 'https://cosmicjs.com/apps/' + invokedCmd + '/demo'
-  if (!demoLink) {
-    print.cosmic('No demo for this app found. Please visit ' + metadata.repo + ' to explore more.' )
-  }
-
   // open in user's default browser
   opn(demoLink)
   process.exit()
