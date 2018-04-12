b53044e0c68415844913027310157142afdee361
Tony Spiro <tspiro@tonyspiro.com>
2018-04-12 13:35:55 -0500

    open app locally

--- non_standard_commands/start-app.js
+++ non_standard_commands/start-app.js
@@ -5,6 +5,7 @@ var async = require('async')
 var exec = require('child_process').exec
 var bucketConfig = require('../lib/bucket_config')
 var bucket_opts = bucketConfig.getCosmicBucketOptions()
+var opn = require('opn')
 
 function handler(options) {
   var invokedCmd = options.invokedCmd
