6d6bb1de07b44732efcaa8fe07edcf191a5a83b6
Robin Jayaswal <robin.e.jayaswal.18@dartmouth.edu>
2018-05-19 09:26:03 -0400

    add bucket sets bucket active

---
+++ non_standard_commands/add-bucket.js
@@ -0,0 +1,46 @@
+var print = require('../lib/output')
+var bucketConfig = require('../lib/bucket_config')
+
+
+function handler(options) {
+  var invokedCmd = options.invokedCmd
+  var Cosmic = options.Cosmic
+
+
+  var params = {
+    slug: invokedCmd.slug,
+    title: invokedCmd.title,
+    cluster: invokedCmd.cluster,
+    object_types: invokedCmd.object_types,
+    objects: invokedCmd.objects,
+    media: invokedCmd.media,
+    media_folders: invokedCmd.media_folders,
+    webhooks: invokedCmd.webhooks,
+    extensions: invokedCmd.extensions,
+    read_key: invokedCmd.read_key,
+    write_key: invokedCmd.write_key
+  }
+
+  var res
+  Cosmic.addBucket(params)
+    .then(function(result) {
+      res = result
+      var bucket = {
+        slug: result.bucket.slug,
+        read_key: params.read_key,
+        write_key: params.write_key
+      }
+      return bucketConfig.setBucket(bucket)
+    })
+    .then(function() {
+      print.success('Success')
+      console.log(res)
+      process.exit()
+    }).catch(function(err) {
+      print.error('Error')
+      console.log(err)
+      process.exit(1)
+    })
+}
+
+module.exports = handler
