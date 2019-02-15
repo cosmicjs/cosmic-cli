29e6e86a7c7062dae92d100b7ebc9ff45d9b9a32
Tony Spiro <tspiro@tonyspiro.com>
2019-02-14 22:18:04 -0800

    rm cross-env

--- non_standard_commands/start-app.js
+++ non_standard_commands/start-app.js
@@ -30,7 +30,7 @@ function handler() {
     function() {
       var PORT = process.env.PORT || 3000
       console.log(bucket_opts.slug)
-      var start_command = 'cd ./' + appSlug + '; cross-env COSMIC_BUCKET=' + bucket_opts.slug + ' PORT=' + PORT + ' npm start;'
+      var start_command = 'cd ./' + appSlug + '; COSMIC_BUCKET=' + bucket_opts.slug + ' PORT=' + PORT + ' npm start;'
       console.log(start_command)
       print.success('Starting app on port number ' + PORT + ' connected to Bucket ' + bucket_opts.slug + '.  Open your browser to http://localhost:3000')
       var child = exec(start_command, function(error, stdout, stderr) {
